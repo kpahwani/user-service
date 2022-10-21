a519279563572b878bb0bf9709bfc481d125af99
Abdul Rahman <abdul.rahman@talentica.com>
2022-10-21 13:28:14 +0530

    added user n follower apis

--- app/helpers/dbHelper/users.js
+++ app/helpers/dbHelper/users.js
@@ -1,14 +1,48 @@
+const Followers = require('../../models/followers');
 const Users = require('../../models/users');
 
 const registerUser = async (data) => {
     return await Users.create(data);
 };
 
+const updateUserById = async (userId, dataToUpdate) => {
+    const options = {
+        new: true
+    };
+    return Users.findByIdAndUpdate(userId, dataToUpdate, options);
+};
+
 const fetchUserByEmail = async (email) => {
     return await Users.findOne({ email });
 };
 
+const getUserById = async (userId) => {
+    return await Users.findOne({ _id: userId });
+};
+
+const followUser = (followerId, followeeId) => {
+    return Followers.create({ followerId, followeeId });
+};
+
+const unfollowUser = (followerId, followeeId) => {
+    return Followers.findOneAndDelete({ followerId, followeeId });
+};
+
+const getFollowers = (userId) => {
+    return Followers.find({ followeeId: userId });
+};
+
+const getFollowing = (userId) => {
+    return Followers.find({ followerId: userId });
+};
+
 module.exports = {
     registerUser,
-    fetchUserByEmail
+    updateUserById,
+    fetchUserByEmail,
+    getUserById,
+    followUser,
+    unfollowUser,
+    getFollowers,
+    getFollowing
 };
--- app/routes/users.js
+++ app/routes/users.js
@@ -9,5 +9,51 @@ usersRoutes.post('/register', asyncRouteHandler(async (req, res) => {
     res.send(registeredUser);
 }));
 
+// update user
+usersRoutes.put('/user/:userId', asyncRouteHandler(async (req, res) => {
+    const userId = req.params.userId;
+    const user = await usersController.updateUser(userId, req.body);
+    res.send(user);
+}));
+
+
+usersRoutes.get('/user/:userId', asyncRouteHandler(async (req, res) => {
+    const userId = req.params.userId;
+    const user = await usersController.getUserDetails(userId);
+    res.send(user);
+}));
+
+
+usersRoutes.post('/follow', asyncRouteHandler(async (req, res) => {
+    const followData = await usersController.followUser(req.body);
+    res.send({
+        msg: 'follow successful',
+        data: followData
+    });
+}));
+
+
+usersRoutes.post('/unfollow', asyncRouteHandler(async (req, res) => {
+    const unfollowData = await usersController.unfollowUser(req.body);
+    res.send({
+        msg: 'unfollow successful',
+        data: unfollowData
+    });
+}));
+
+
+usersRoutes.get('/followers/:userId', asyncRouteHandler(async (req, res) => {
+    const userId = req.params.userId;
+    const followers = await usersController.getFollowers(userId);
+    res.send(followers);
+}));
+
+
+usersRoutes.get('/following/:userId', asyncRouteHandler(async (req, res) => {
+    const userId = req.params.userId;
+    const followings = await usersController.getFollowing(userId);
+    res.send(followings);
+}));
+
 
 module.exports = usersRoutes;
